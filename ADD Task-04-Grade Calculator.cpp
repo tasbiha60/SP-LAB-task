49aece4286a3288337c12369e4a9cc3f07cd2cf4
tasbiha60 <htjtasbi@gmail.com>
2024-10-01 23:29:07 +0600

    Create ADD Task-04-Grade Calculator.cpp
    Problem: Grade Calculator
    Write a program that takes a score as input and assigns a grade (A, B, C, D, F) using the switch-case statement.

---
+++ ADD Task-04-Grade Calculator.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int m;
+scanf("%d",&m);
+switch(m/10){
+case 10:printf("A\n");
+break;
+case 9:printf("B\n");
+break;
+case 8:printf("C\n");
+break;
+case 7:printf("D\n");
+break;
+case 6:printf("F\n");
+break;
+default:
+printf("Invalid Operation \n");
+}
+return 0;
+}
